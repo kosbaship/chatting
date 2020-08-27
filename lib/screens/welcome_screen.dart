cc9f611d1248b3a5b62cf92bf23b09b84647dbec
kosbaship <mohamed.kosba96@gmail.com>
2020-08-27 15:24:06 +0200

    كستم انميشن فيه تكر وكونترولر وفاليو -- مثال تغير شفافيه الخلفيه

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -10,11 +10,51 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+// هنضيف السطر دا للولكم اسكرين علشان ياكت كا تكر بروفايدر (التكه هيا قلبه الصفحه ف الفليب بوك)
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+
+   // هنعرف الانميشن كونترولر
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // هنعمل التهيئه بتاعته انه يتبني اول ما الاستات اوبجكت بتعنا يتبني
+    //  بشكل افتراضي الكونرولر دا هيحرك رقم والرقم دا بيزيد مع كل تكه
+    //  والرقم دا بيبق في مدي بين صفر لواحد 0.01
+    //  وطبعا في الثانيه الواحده هنلاقي ٦٠ تكه في التكير بتاعنا بمعني
+    //  تاني الكونترولر بتاعنا هيتحرك من ٠ لواحده في ٦٠ خطوه
+    controller = AnimationController(
+      //الانميشن دا هيعد وقت قد ايه
+      duration: Duration(seconds: 1),
+      // دي المكان اللي بنحط فيه التيكر بروفايدر وفي حالتنا هنا
+      // احنا هنستخدم الكلاس بتاعنا (ستات اوبجكت) كتيكر بوفايدر
+      vsync: this,    // دي الوحيده المطلوبه بشكل اجباري علشان يحدد ايه اللي هيبق تكر ======>
+    );
+
+    //كدا نخلي الانميشن يبدا
+    //دي هتخلي الانميش يكمل
+    controller.forward();
+
+    // هنضيف مستمع للكونرولر علشان نشوف هو بيعمل ايه
+    // معلومه تخص الليسنر انه بياخد جواه كول باك
+    controller.addListener(() {
+      // دي هنضيفها علشان خاطر نخلي فلاتر تتفاعل مع القيمه اللي بتتغير
+      // وانها تعد تبني الاسكرين من اول جديد
+      setState(() {
+        // احنا سايبنها فاضيه لان القيمه بتاعتنا بالفعل بتتغير جوه الانميشن كونترولر
+      });
+      // هنا احنا ممكن نتصنت علي القيمه اللي جوه الكونرولر
+      print(controller.value);
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      // هنا انا ممكن استخدم القيمه اللي جوه الكونرولر دا علشان اغير بيها حاجه مثال
+      backgroundColor: Colors.white.withOpacity(controller.value),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
